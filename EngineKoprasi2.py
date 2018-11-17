b877df64238c8e7df110d2f7604c78be358fce2e
Gustut Biksa <subargawa01@gmail.com>
2018-11-17 19:55:10 +0800

    start engine koprasi 2

---
+++ EngineKoprasi2.py
@@ -0,0 +1,9 @@
+import mysql.connector
+import datetime
+from prettytable import PrettyTable
+import pandas as panda
+from MDI.TugasAkhir import Koprasi1_Function
+
+con_database = mysql.connector.connect(user="root", host="localhost", database="db_koprasi1")
+cur_database = con_database.cursor(buffered=True)
+
