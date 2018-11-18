6b4c30fd1cfc8685bed470e5a555ffb85b4a7079
Gustut Biksa <subargawa01@gmail.com>
2018-11-18 21:37:15 +0800

    finishing export data to Json, - belum melakukan konek ke db_koprasi2 - blm sempurna pada ranah database

--- EngineKoprasi2.py
+++ EngineKoprasi2.py
@@ -1,9 +1,14 @@
 import mysql.connector
 import datetime
-from prettytable import PrettyTable
-import pandas as panda
-from MDI.TugasAkhir import Koprasi1_Function
+from MDI.TugasAkhir import Koprasi2_Function
 
+#Koneksikan database berupa "user","host" dan "database" pada db_koprasi2 dimana adalah database koprasi kedua
+# dan memasukkannya kedalam variabel "con_database"
+#Memanggil perintah cursor pada variabel "con_database" dan menyimpannya dalam variabel "cur_database"
 con_database = mysql.connector.connect(user="root", host="localhost", database="db_koprasi1")
 cur_database = con_database.cursor(buffered=True)
 
+#Deklarasikan variabel "exportJson" dengan nilai awal 0 yang nantinya akan digunakan oleh engine "Koprasi2_Function" sebagai perulangan
+#Memanggil fungsi "ExportDataToJson" pada engine "Koprasi2_Function" dengan parameter variabel "cur_database" dan "exportJson"
+exportJson = 0
+exportJson = Koprasi2_Function.ExportDataToJson(cur_database,exportJson)
