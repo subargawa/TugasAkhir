ed0522c6d88fb7906d3ffc8abddd94907e54dfb9
Gustut Biksa <subargawa01@gmail.com>
2018-11-21 20:15:53 +0800

    Perbaikan di variabel EnKop 1 dan 2

--- EngineKoprasi1.py
+++ EngineKoprasi1.py
@@ -1,10 +1,9 @@
 import mysql.connector
 import datetime
-from prettytable import PrettyTable
-import pandas as panda
 from MDI.TugasAkhir import Koprasi1_Function
 
 con_database = mysql.connector.connect(user="root", host="localhost", database="db_koprasi1")
 cur_database = con_database.cursor(buffered=True)
 
-exportExcel= Koprasi1_Function.exportDataToExcel(con_database, exportExcel)
+exportExcel=0
+exportExcel= Koprasi1_Function.exportDataToExcel(con_database, cur_database, exportExcel)
--- Koprasi1_Function.py
+++ Koprasi1_Function.py
@@ -1,13 +1,7 @@
-import mysql.connector
 import datetime
-from prettytable import PrettyTable
 import pandas as panda
 
-con_database = mysql.connector.connect(user="root", host="localhost", database="db_koprasi1")
-cur_database = con_database.cursor(buffered=True)
-
-
-def exportDataToExcel(con_database, exportExcel):
+def exportDataToExcel(con_database, cur_database, exportExcel):
     #Menampilkan Seluruh Table
     cur_database.execute("SHOW TABLES")
     tablesList= cur_database.fetchall()
@@ -16,13 +10,9 @@ def exportDataToExcel(con_database, exportExcel):
     for tables in tablesList:
         if len(tablesList)>exportExcel:
             tableName=tables[0]
-            # print(tableName)
-            cur_database.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s'" %(tableName))
-            colomnTable= cur_database.fetchall()
-            print(colomnTable[2])
 
-            a=panda.read_sql("SELECT * FROM %s"  %(tableName), con_database)
-            a.to_excel('D:ims'+str(exportExcel)+'.xls')
+            writeExcel=panda.read_sql("SELECT * FROM %s"  %(tableName), con_database)
+            writeExcel.to_excel('D:ims'+str(exportExcel)+'.xls')
             exportExcel= exportExcel+1
 
         else:
