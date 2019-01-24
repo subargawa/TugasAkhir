59e6ce751e731974ace62f339681c89529fbbcfa
Gustut Manuaba <subargawa01@gmail.com>
2019-01-24 13:58:06 +0800

    Delete EngineKoprasi1.py

--- EngineKoprasi1.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-import datetime
-from MDI.TugasAkhir import Koprasi1_Function
-
-con_database = mysql.connector.connect(user="root", host="localhost", database="db_koprasi1")
-cur_database = con_database.cursor(buffered=True)
-
-exportExcel=0
-exportExcel= Koprasi1_Function.exportDataToExcel(con_database, cur_database, exportExcel)
